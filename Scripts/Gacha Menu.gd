b18d65da3ad2d8f59a1ff3d3b3737746c784ef17
Lowliet <lowdata114@gmail.com>
2024-02-29 08:47:05 -0800

    Updated Gacha system
    with animation and sound

--- Scripts/Gacha Menu.gd
+++ Scripts/Gacha Menu.gd
@@ -8,13 +8,20 @@ var new_poten_input = 1
 var former_poten_input
 
 @onready var highlight_effect = $highlight_button
+enum Gacha_Menu_States {Gacha_Menu, Rolling_Menu}
+var current_gacha_menu_state : Gacha_Menu_States = Gacha_Menu_States.Gacha_Menu
+
+@onready var gacha_system = $"Gacha System"
 
 
 func _ready():
 	#initialize highlight effect
 	update_highlight_position()
-	
+	var gacha_system_node = $"Gacha System"
+	gacha_system_node.connect("end_gacha", reset_menu_state)
 	former_poten_input = new_poten_input
+	
+	
 
 
 func _input(event):
@@ -32,22 +39,28 @@ func _input(event):
 	elif (event.is_action_pressed("5_key_push")):
 		num = 5
 	
-	if num == 0 or num == 1 or num == 2:
-		selected_button_index = 0
-	elif num == 3 or num == 4 or num == 5:
-		selected_button_index = 1
-	
-	if num != -1:
-		update_highlight_position()
-	# Button: Accept / Confirm
-	elif (event.is_action_pressed("right_button")):
-		press_selected_button(selected_button_index)
+	if(current_gacha_menu_state == Gacha_Menu_States.Gacha_Menu):
+		if num == 0 or num == 1 or num == 2:
+			selected_button_index = 0
+		elif num == 3 or num == 4 or num == 5:
+			selected_button_index = 1
+	if(current_gacha_menu_state == Gacha_Menu_States.Gacha_Menu):
+		if num != -1:
+			update_highlight_position()
+		# Button: Accept / Confirm
+		elif (event.is_action_pressed("right_button")):
+			press_selected_button(selected_button_index)
 		
-		# DEBUG: Gain more coins
-	elif(event.is_action_pressed("ui_accept")):
-		GameManager.total_coins += 1
-		print("Total Coins: " + str(GameManager.total_coins))
+			# DEBUG: Gain more coins
+		elif(event.is_action_pressed("ui_accept")):
+			GameManager.total_coins += 1
+			print("Total Coins: " + str(GameManager.total_coins))
 
+func reset_menu_state():
+	current_gacha_menu_state = Gacha_Menu_States.Gacha_Menu
+	selected_button_index = 1
+	update_highlight_position()
+	$"Gacha System/Pal Sprite".texture = load("res://Sprites/capsule.png")
 
 func update_highlight_position():
 	var selected_button = get_node("Buttons").get_child(selected_button_index)
@@ -61,9 +74,12 @@ func press_selected_button(index):
 		1:
 			_on_roll_button_pressed()
 
-
 func _on_back_button_pressed():
 	get_tree().change_scene_to_file("res://Scenes/Main Menu.tscn")
 
 func _on_roll_button_pressed():
-	emit_signal("start_gacha")
+	if(GameManager.total_coins >= gacha_system.roll_cost):
+		current_gacha_menu_state = Gacha_Menu_States.Rolling_Menu
+		emit_signal("start_gacha")
+	else:
+		print("Not enough coins")
--- Scripts/Gacha System.gd
+++ Scripts/Gacha System.gd
@@ -1,29 +1,120 @@
 extends Node
 
+signal end_gacha
+
 var roll_cost = 2
 var old_pal = null
 
+var rolling_state = false
+var coin_inserted = false
+var crank_set = false
+var finished_rolling = false
+
 @onready var gacha_menu_node = $".."
+@onready var capsule_machine_sprite = $"Capsule Machine Sprite"
 @onready var pal_sprite = $"Pal Sprite"
 @onready var coins_label = $"Coins Available"
+@onready var anim_player = $AnimationPlayer
+@onready var audio_player = $AudioStreamPlayer2D
+
+var audio_stream : AudioStream
+var mon_get_sound = "res://Sounds/mon_get.wav"
+var gacha_crank_sound = "res://Sounds/crank.wav"
+var coin_insert_sound = "res://Sounds/coin_insert.wav"
+var rolling_sound = "res://Sounds/rolling2.wav"
+
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#initialize gacha signal and base spprite
-	gacha_menu_node.connect("start_gacha", gacha_roll)
+	gacha_menu_node.connect("start_gacha", set_rolling_menu)
+	set_gacha_menu()
 	pal_sprite.texture = load("res://Sprites/capsule.png")
 	coins_label.text = "Coins Owned: " + str(GameManager.total_coins)
-	
-	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	coins_label.text = "Coins Owned: " + str(GameManager.total_coins)
+
+func _input(event):
+	if(rolling_state == true && finished_rolling == false):
+		if(event.is_action_pressed("left_button")):
+			coin_inserted = true
+			audio_stream = load(coin_insert_sound)
+			audio_player.stream = audio_stream
+			audio_player.play()
+			print("Coin Inserted")
+		if(event.is_action_pressed("0_key_push")):
+			crank_set = true
+			audio_stream = load(gacha_crank_sound)
+			audio_player.stream = audio_stream
+			audio_player.play()
+			print("Crank Set")
+		if(crank_set == true && coin_inserted == true):
+			if(event.is_action_pressed("5_key_push")):
+				audio_stream = load(rolling_sound)
+				audio_player.stream = audio_stream
+				audio_player.play()
+				gacha_roll()
+	if(finished_rolling == true):
+		if(event.is_action_pressed("left_button")):
+			pal_sprite.self_modulate = Color(1, 1, 1, 1)
+			set_gacha_menu()
+			finished_rolling = false
+				
+			emit_signal("end_gacha")
+
+	
+func set_rolling_menu():
+	rolling_state = true
+	$"Crank Dial Text".visible = true
+	$"Dial Sprite".visible = true
+	$"Coins to Roll".visible = false
+	$"Coins Available".visible = false
+	$"../Buttons/Roll Button".visible = false
+	$"../highlight_button".visible = false
+	$"Old Mon Acquired".visible = false
+	$"New Mon Acquired".visible = false
+	
+	capsule_machine_sprite.visible = false
+	pal_sprite.visible = false
+	pal_sprite.texture = load("res://Sprites/capsule.png")
+	
+func set_gacha_menu():
+	rolling_state = false
+	$"Crank Dial Text".visible = false
+	$"Dial Sprite".visible = false
+	$"Coins to Roll".visible = true
+	$"Coins Available".visible = true
+	$"../Buttons/Roll Button".visible = true
+	$"../highlight_button".visible = true
+	$"Old Mon Acquired".visible = false
+	$"New Mon Acquired".visible = false
+	if(old_pal != null):
+		old_pal.visible = false
+	capsule_machine_sprite.visible = true
+	pal_sprite.visible = true
+	pal_sprite.texture = load("res://Sprites/capsule.png")
+	
+	emit_signal("end_gacha")
 	
 
 func gacha_roll():
 	print("Rolling")
+	
+	#-----Play capsule animation
+	$"Crank Dial Text".visible = false
+	$"Dial Sprite".visible = false
+	pal_sprite.texture = load("res://Sprites/capsule.png")
+	anim_player.play("capsule_roll")
+	await get_tree().create_timer(1.1).timeout
+	
+	audio_stream = load(mon_get_sound)
+	audio_player.stream = audio_stream
+	audio_player.play()
+	
+	
 	if(GameManager.pals_inventory.size() >= 6):
 		$"All Mons Obtained".visible = true
 		return
@@ -47,9 +138,14 @@ func gacha_roll():
 		#store the index of the pal so can call it from Pal List
 		if !GameManager.pals_inventory.has(GameManager.pal_list[random_index]):
 			GameManager.pals_inventory.append(GameManager.pal_list[random_index])
+			$"New Mon Acquired".visible = true
+		else:
+			$"Old Mon Acquired".visible = true
 		
 		#Adjust Coins
 		GameManager.total_coins -= roll_cost
+		
+		finished_rolling = true
 		
 		# Debug
 		print("Got new Pal")
